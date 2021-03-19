a5ce2dfd0cb676d8b6418f61a30754a7f0953a49
tai_yaki_33 <kuro37.taku@gmail.com>
2021-03-20 02:19:59 +0900

    added source code

---
+++ src/background.js
@@ -0,0 +1,69 @@
+chrome.runtime.onInstalled.addListener(() => {
+    //親メニュー
+    const parentId = chrome.contextMenus.create({
+        title : "Huga Extension",
+        id : "parent",
+        type : "normal",
+        contexts : ["all"]
+    });
+
+    //子メニュー
+    const childId = chrome.contextMenus.create({
+        title : "切り替え",
+        id : "child",
+        type : "checkbox",
+        contexts : ["all"],
+        parentId : "parent"
+    });
+
+    const clip = chrome.contextMenus.create({
+        title : "選択範囲をクリップボードに保存",
+        id : "clip",
+        type : "normal",
+        contexts : ["selection"],
+        parentId : "parent"
+    });
+    
+    console.log("Extension was installed!");
+});
+
+
+//コンテキストメニュー押下時の動作
+chrome.contextMenus.onClicked.addListener((info) => {
+    if(info.menuItemId == "clip"){
+        const selected = info.selectionText;
+        if(selected != null){
+            console.log(selected);
+            navigator.clipboard.writeText(selected);
+            chrome.storage.local.set({"selected" : selected});
+        }
+
+        const clipped = chrome.contextMenus.create({
+            title : selected,
+            id : "clipped",
+            type : "normal",
+            contexts : ["all"],
+            parentId : "parent"
+        });
+
+        //クリップボード削除
+        const deleteClipboard = chrome.contextMenus.create({
+            title : "クリップボードを削除",
+            id : "delete",
+            type : "normal",
+            contexts : ["all"],
+            parentId : "parent"
+        });
+    };
+    if(info.menuItemId == "delete"){
+        chrome.contextMenus.remove("delete");
+        chrome.contextMenus.remove("clipped", () => {
+            console.log("Clipped text was deleted!");
+        });
+    }
+    if(info.menuItemId == "clipped"){
+        chrome.storage.local.get("clipped", (data) => {
+            console.log(data);
+        });
+    }
+});
